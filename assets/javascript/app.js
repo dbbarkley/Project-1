1dce5f8fb34d628e3101eb57933c9d842d6fd409
Ashley <ashleymwilloughby@gmail.com>
2019-02-02 13:26:07 -0600

    added firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCvgmtREtDbLoWfqqa6-v5VrykBXjzy9FI",
+    authDomain: "foodflix-9170b.firebaseapp.com",
+    databaseURL: "https://foodflix-9170b.firebaseio.com",
+    projectId: "foodflix-9170b",
+    storageBucket: "",
+    messagingSenderId: "471943646046"
+};
+firebase.initializeApp(config);
