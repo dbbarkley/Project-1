1763baab5bc2bad1e26feb800ae2d539602e71b2
Ashley <ashleymwilloughby@gmail.com>
2019-02-07 20:49:14 -0600

    added button class in index

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,7 +10,6 @@ var x = Math.floor(Math.random() * 256);
 var y = Math.floor(Math.random() * 256);
 var z = Math.floor(Math.random() * 256);
 var bgColor = "rgb(" + x + "," + y + "," + z + ")";
-  console.log(bgColor);
 $("body, html").css("background-color", bgColor);
 }
 
