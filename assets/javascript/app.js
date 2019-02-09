9dd391ed1e561948c450d45b932eff6742e0456a
Ashley <ashleymwilloughby@gmail.com>
2019-02-09 11:32:32 -0600

    added radio buttons and updated the colors function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,14 +5,28 @@ $(document).ready(function() {
   });
 });
 
+var bgColors = ["46,139,87", "30,144,255", "60,179,113" , "255,0,255", "245,176,203", "220,106,207", 
+                "212,77,92", "148, 197, 204", "180, 210, 231", "169, 206, 244", "34, 174, 209", 
+                "175, 169, 141", "105, 153, 93", "132, 192, 198", "70, 177, 201", "207, 142, 128", 
+                "60, 136, 110", "239, 136, 227", "103, 142, 54", "221, 162, 79", "193, 77, 148",
+                "160, 157, 165", "87, 222, 193", "27, 173, 196", "240, 153, 81", "48, 197, 136", 
+                "3, 198, 214"];
 
 function random_bg_color() {
-var x = Math.floor(Math.random() * 256);
-var y = Math.floor(Math.random() * 256);
-var z = Math.floor(Math.random() * 256);
-var bgColor = "rgb(" + x + "," + y + "," + z + ")";
-$("body, html").css("background-color", bgColor);
-}
+  var randomColor = bgColors[Math.floor(Math.random()*bgColors.length)];
+  $("body, html").css("background-color", "rgb(" + randomColor + ")");
+};
+
+// This code chooses random color from the whole RGB spectrum, used to choose which ones looked nice
+  // function random_bg_color() {
+  // var x = Math.floor(Math.random() * 256);
+  // var y = Math.floor(Math.random() * 256);
+  // var z = Math.floor(Math.random() * 256);
+  // var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+  //   console.log(bgColor);
+  // $("body, html").css("background-color", bgColor);
+  // }
+
 
 // Initialize Firebase
 var config = {
