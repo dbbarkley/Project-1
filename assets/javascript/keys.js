1a8a0c89bd0ebb42ab251b1516af6b660c5a9ad4
starbuck <mausuf@utexas.edu>
2019-02-09 13:33:53 -0600

    resized the food results, please remove the API Key js before merging

---
+++ assets/javascript/keys.js
@@ -0,0 +1,12 @@
+var firebaseKey = "AIzaSyCvgmtREtDbLoWfqqa6-v5VrykBXjzy9FI"
+
+var edamamKey = {
+ ashleyID: "19663c34",
+ ashleyKey: "d5b225ff367759093464486ef7cd8246",
+ danielID: "8404cc17",
+ danielKey: "295468265fe5d24e7c933c96747b1f2d",
+ mausufID: "b8c0588e",
+ mausufKey: "0d7c770333b36673161dac5477aaaa86",
+ oliverID: "907fcd7c",
+ oliverKey: "c5117c4a65bdb06f4ea9592650475c7a"
+};
